299d504546ef18da9d331cabdacc3ab7ba5dfa4f
emnlp-1317 <143490192+emnlp-1317@users.noreply.github.com>
2023-08-29 17:53:43 +1000

    Add files via upload

---
+++ CL-TSQA-baseline/CL-TSQA/FiD/prompt_t5/t5_model/__init__.py
@@ -0,0 +1,3 @@
+from .configuration_t5 import T5Config
+from .tokenization_t5 import T5Tokenizer
+from .modeling_t5 import T5ForConditionalGeneration
---
+++ CL-TSQA-baseline/CL-TSQA/scripts/fid_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+python -m FiD.main mode=train dataset=cltsqa model_path=checkpoints/fid/cl_one/checkpoint-epoch-0/
+
